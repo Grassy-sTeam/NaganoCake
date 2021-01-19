21c79a4246646aa4c11f370e20676c680adc13fb
TOMOKIHAGUI <higosozan18@gmail.com>
2021-01-19 07:14:04 +0000

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :customers
-  root to: 'homes#top'
-  get 'homes/about'
+
+    root to: 'homes#top'
+    get 'homes/about'
+    devise_for :customers
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
